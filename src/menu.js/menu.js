e4ebbc8b48bf0ac6c27f8b5ef146fbfd957c3ee7
VitaliyYakovlev <vitaliy-yakovlev@ukr.net>
2021-02-22 08:51:03 +0200

    Убрал xlink:

--- src/menu.js/menu.js
+++ src/menu.js/menu.js
@@ -5,13 +5,13 @@
   menuBtnRef.addEventListener('click', () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
+    
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
     document.body.classList.toggle("modal-open");
+
   });
 })();
-
 
