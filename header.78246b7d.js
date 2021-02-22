e6cc11ca508c5bfb6ef13bef5156080cbfadeee9
Vladyslav <vladtravel2015@gmail.com>
2021-02-22 18:32:25 +0200

    Updates

--- header.78246b7d.js
+++ header.78246b7d.js
@@ -130,19 +130,17 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
-})();
-
-(function () {
-  document.querySelector('.js-speaker-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    new FormData(e.currentTarget).forEach(function (value, name) {
-      return console.log("".concat(name, ": ").concat(value));
-    });
-    e.currentTarget.reset();
-  });
-})();
+})(); // (() => {
+//   document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+//     e.preventDefault();
+//     new FormData(e.currentTarget).forEach((value, name) =>
+//       console.log(`${name}: ${value}`),
+//     );
+//     e.currentTarget.reset();
+//   });
+// })();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -171,7 +169,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55571" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50129" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
