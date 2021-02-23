f881acba90751681c993553916a2c49dab8b8389
Vladyslav <vladtravel2015@gmail.com>
2021-02-23 12:09:27 +0200

    Updates

--- slider.ab09d017.js
+++ slider.ab09d017.js
@@ -118,41 +118,239 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"slider.js":[function(require,module,exports) {
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName('sliderbox');
+//   var dots = document.getElementsByClassName('slider-dots_item');
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none';
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(' active', '');
+//   }
+//   slides[slideIndex - 1].style.display = 'block';
+//   dots[slideIndex - 1].className += ' active';
+// }
+function Sim(sldrId) {
+  var id = document.getElementById(sldrId);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+  if (id) {
+    this.sldrRoot = id;
+  } else {
+    this.sldrRoot = document.querySelector('.sim-slider');
+  } // Carousel objects
+
+
+  this.sldrList = this.sldrRoot.querySelector('.sim-slider-list');
+  this.sldrElements = this.sldrList.querySelectorAll('.sim-slider-element');
+  this.sldrElemFirst = this.sldrList.querySelector('.sim-slider-element');
+  this.leftArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-left');
+  this.rightArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-right');
+  this.indicatorDots = this.sldrRoot.querySelector('div.sim-slider-dots'); // Initialization
+
+  this.options = Sim.defaults;
+  Sim.initialize(this);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+Sim.defaults = {
+  // Default options for the carousel
+  loop: true,
+  // Бесконечное зацикливание слайдера
+  auto: true,
+  // Автоматическое пролистывание
+  interval: 5000,
+  // Интервал между пролистыванием элементов (мс)
+  arrows: true,
+  // Пролистывание стрелками
+  dots: true // Индикаторные точки
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName('sliderbox');
-  var dots = document.getElementsByClassName('slider-dots_item');
+};
 
-  if (n > slides.length) {
-    slideIndex = 1;
+Sim.prototype.elemPrev = function (num) {
+  num = num || 1;
+  var prevElement = this.currentElement;
+  this.currentElement -= num;
+  if (this.currentElement < 0) this.currentElement = this.elemCount - 1;
+
+  if (!this.options.loop) {
+    if (this.currentElement == 0) {
+      this.leftArrow.style.display = 'none';
+    }
+
+    this.rightArrow.style.display = 'block';
   }
 
-  if (n < 1) {
-    slideIndex = slides.length;
+  this.sldrElements[this.currentElement].style.opacity = '1';
+  this.sldrElements[prevElement].style.opacity = '0';
+
+  if (this.options.dots) {
+    this.dotOn(prevElement);
+    this.dotOff(this.currentElement);
+  }
+};
+
+Sim.prototype.elemNext = function (num) {
+  num = num || 1;
+  var prevElement = this.currentElement;
+  this.currentElement += num;
+  if (this.currentElement >= this.elemCount) this.currentElement = 0;
+
+  if (!this.options.loop) {
+    if (this.currentElement == this.elemCount - 1) {
+      this.rightArrow.style.display = 'none';
+    }
+
+    this.leftArrow.style.display = 'block';
   }
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+  this.sldrElements[this.currentElement].style.opacity = '1';
+  this.sldrElements[prevElement].style.opacity = '0';
+
+  if (this.options.dots) {
+    this.dotOn(prevElement);
+    this.dotOff(this.currentElement);
+  }
+};
+
+Sim.prototype.dotOn = function (num) {
+  this.indicatorDotsAll[num].style.cssText = 'background-color:#BBB; cursor:pointer;';
+};
+
+Sim.prototype.dotOff = function (num) {
+  this.indicatorDotsAll[num].style.cssText = 'background-color:#556; cursor:default;';
+};
+
+Sim.initialize = function (that) {
+  // Constants
+  that.elemCount = that.sldrElements.length; // Количество элементов
+  // Variables
+
+  that.currentElement = 0;
+  var bgTime = getTime(); // Functions
+
+  function getTime() {
+    return new Date().getTime();
   }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
+  function setAutoScroll() {
+    that.autoScroll = setInterval(function () {
+      var fnTime = getTime();
+
+      if (fnTime - bgTime + 10 > that.options.interval) {
+        bgTime = fnTime;
+        that.elemNext();
+      }
+    }, that.options.interval);
+  } // Start initialization
+
+
+  if (that.elemCount <= 1) {
+    // Отключить навигацию
+    that.options.auto = false;
+    that.options.arrows = false;
+    that.options.dots = false;
+    that.leftArrow.style.display = 'none';
+    that.rightArrow.style.display = 'none';
   }
 
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-}
+  if (that.elemCount >= 1) {
+    // показать первый элемент
+    that.sldrElemFirst.style.opacity = '1';
+  }
+
+  if (!that.options.loop) {
+    that.leftArrow.style.display = 'none'; // отключить левую стрелку
+
+    that.options.auto = false; // отключить автопркрутку
+  } else if (that.options.auto) {
+    // инициализация автопрокруки
+    setAutoScroll(); // Остановка прокрутки при наведении мыши на элемент
+
+    that.sldrList.addEventListener('mouseenter', function () {
+      clearInterval(that.autoScroll);
+    }, false);
+    that.sldrList.addEventListener('mouseleave', setAutoScroll, false);
+  }
+
+  if (that.options.arrows) {
+    // инициализация стрелок
+    that.leftArrow.addEventListener('click', function () {
+      var fnTime = getTime();
+
+      if (fnTime - bgTime > 1000) {
+        bgTime = fnTime;
+        that.elemPrev();
+      }
+    }, false);
+    that.rightArrow.addEventListener('click', function () {
+      var fnTime = getTime();
+
+      if (fnTime - bgTime > 1000) {
+        bgTime = fnTime;
+        that.elemNext();
+      }
+    }, false);
+  } else {
+    that.leftArrow.style.display = 'none';
+    that.rightArrow.style.display = 'none';
+  }
+
+  if (that.options.dots) {
+    (function () {
+      // инициализация индикаторных точек
+      var sum = '',
+          diffNum;
+
+      for (var i = 0; i < that.elemCount; i++) {
+        sum += '<span class="sim-dot"></span>';
+      }
+
+      that.indicatorDots.innerHTML = sum;
+      that.indicatorDotsAll = that.sldrRoot.querySelectorAll('span.sim-dot'); // Назначаем точкам обработчик события 'click'
+
+      var _loop = function _loop(n) {
+        that.indicatorDotsAll[n].addEventListener('click', function () {
+          diffNum = Math.abs(n - that.currentElement);
+
+          if (n < that.currentElement) {
+            bgTime = getTime();
+            that.elemPrev(diffNum);
+          } else if (n > that.currentElement) {
+            bgTime = getTime();
+            that.elemNext(diffNum);
+          } // Если n == that.currentElement ничего не делаем
+
+        }, false);
+      };
+
+      for (var n = 0; n < that.elemCount; n++) {
+        _loop(n);
+      }
+
+      that.dotOff(0); // точка[0] выключена, остальные включены
+
+      for (var _i = 1; _i < that.elemCount; _i++) {
+        that.dotOn(_i);
+      }
+    })();
+  }
+};
+
+new Sim();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -181,7 +379,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58055" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58828" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
