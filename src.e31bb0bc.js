8aef1a86402f2e8893a948ef291749f418430c15
Vladyslav <vladtravel2015@gmail.com>
2021-02-23 07:27:33 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53428" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54280" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
